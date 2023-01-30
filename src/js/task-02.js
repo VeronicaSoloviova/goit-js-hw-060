301a6ea5ec5589685ea49971ec6538be2a05e8d4
Veronica Soloviova <nika.sv.work@gmail.com>
2023-01-30 23:03:49 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+console.log(listEl);
+
+function createMarkUp(ingredients) {
+  return ingredients.map(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    li.classList.add('item');
+    
+    return li;
+  })
+}
+
+// console.log(createMarkUp(ingredients));
+
+listEl.append(...createMarkUp(ingredients))
